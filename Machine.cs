58f3b82e7a4a349cf689e083481ce597b7d6d9f1
Domingo Osorio <vindaloo.gamedev@gmail.com>
2019-01-16 22:49:28 +0100

    Fixed ExitStep and using coupled states
    Tested with coupled states
    States will be decoupled next versions to be able to use the same states in different machines

--- Machine.cs
+++ Machine.cs
@@ -2,19 +2,21 @@
     public abstract class Machine<TData> : IStateEvaluator<TData> {
         protected int ActiveState { get; private set; }
 
-        protected abstract IStateEvaluator<TData> this[int x, int y] { get; }
+        protected abstract IStateEvaluator<TData> this[int x] { get; }
 
         public abstract void OnStart(TData data);
 
-        protected abstract int ExitStep(int nextState);
+        protected abstract int ExitStep(TData data, int nextState);
 
         public int Next(TData data) {
             for (; ; ) {
-                ActiveState = this[ActiveState, 0].Next(data);
-                if (ActiveState > 0) { this[ActiveState, 0].OnStart(data); }
+                ActiveState = this[ActiveState].Next(data);
+                if (ActiveState > 0) {
+                    this[ActiveState].OnStart(data);
+                }
                 else { break; }
             }
-            if (ActiveState < 0) { ActiveState = ExitStep(-ActiveState); }
+            if (ActiveState < 0) { ActiveState = ExitStep(data, -ActiveState); }
             return ActiveState;
         }
 

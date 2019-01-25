bf1ef08e246ade5f7a6d5c29adc8b7b62e8f4ef6
Domingo Osorio <vindaloo.gamedev@gmail.com>
2019-01-25 10:38:21 +0100

    Translation and little update
    Translation to english of HFM class and update of state documentation.

--- Machine.cs
+++ Machine.cs
@@ -1,42 +1,49 @@
 ﻿using System;
 
-namespace HFM {
+namespace HFM
+{
     /// <summary>
-    /// Esta clase define un nódo de las máquinas de estado jerárquicas. Estos nodos pueden usarse como
-    /// la raíz o un estado intermedio dentro de la jerarquía, usando los estados como nodos hoja.
+    /// This class define a node of hierarchical state machines. These nodes are used as the root or
+    /// internal node of the tree type hierarchy, leaf nodes will be the states.
     /// 
-    /// Fases de comportamiento:
-    ///  + Fase inicial: Ejecutando OnStart Se prepara para el primer estado que coindice con el estado
-    ///    de entrada.
+    /// Behavior phase:
+    ///  + Initial phase: Executing OnStart it prepares the data for the first state that match with 
+    ///    the entry state.
+    ///  
+    ///  + Loop phase:  In this phase every time the next method is called, the current state of the 
+    ///    logic is updated, being able to change active state and return to a previous node of the 
+    ///    hierarchy. 
     ///    
-    ///  + Fase en bucle: En esta fase cada vez que se llama a next el estado actual de la lógica se 
-    ///    actualiza pudiendo cambiar de estado y volver a un nodo anterior de la jerarquía.
+    ///  + End phase: Due to the values ​​returned by the child nodes, the process ends and this node
+    ///    returns a value that defines the ending the process has had.
     ///    
-    ///  + Fase final: Debido a los valores devueltos por los nodos hijos el proceso termina y este 
-    ///    nodo devuelve un valor que define el tipo de término que ha tenido el proceso.
-    /// 
-    /// Comportamiento:
-    ///    El proceso de inicialización está asociado directamente con OnStart, cuando se ejecuta el 
-    ///    método OnStart se asocia el primer estado como el 0 y luego se llama al OnStart del hijo,
-    ///    de modo que depende de donde se llame a base.OnStart el orden de adaptación al primer estado
-    ///    sucederá en un orden u otro.
+    ///  Behavior:
+    ///    The initialization process is directly associated with OnStart, when the OnStart method 
+    ///    is executed, the first state is associated with 0 and then the child's OnStart is called,
+    ///    so it depends on where base.OnStart is called the adaptation order to the First state will
+    ///    happen in one order or another.
     ///    
-    ///    El el proceso de ejecución se cubren las fases de bucle y final funciona de la siguiente manera.
-    ///    Al llamarse a Next este nodo llama al Next del nodo activo y dependiendo del valor de retorno
-    ///    este nodo actuará de una forma u otra.
-    ///    El valor obtenido valorDeTransición (inicial) no puede ser negativo, los valores viables son:
-    ///     + Si valorDeTransición igualQue 0: el estado activo se mantiene.
-    ///     + Si valorDeTransición mayorQue 0: el valor devuelto se consulta en la tabla de transición y se
-    ///       obtiene el valorDeTransición (final).
-    ///       - Si el valorDeTransición igualQue 0: el estado activo se mantiene.
-    ///       - Si el valorDeTransición menorQue 0: Es una transición de salida y se sale de este nodo de
-    ///         transición con el valor devuelto con ExitStep al que se le pasa el valorDeTransición con 
-    ///         signo cambiado (de esta forma el nodo padre elegirá el estado con su tabla de transición).
-    ///         En caso de que el valor devuelto sea de la raíz de la estructura este valor de salida puede
-    ///         servir para recordar desde que estado se ha salido si es que esto es importante.
-    ///       - Si el valorDeTransición mayorQue 0: Es una transición interna por lo que cambiará de 
-    ///         nodo activo y se llamará a OnStart para inicializarlo antes de ser llamado en el mismo 
-    ///         ciclo de ejecución.
+    ///    The execution process covers the loop and final phases works as follows.
+    ///    When called to Next this node calls the Next of the active node and depending on the return
+    ///    value this node will act in one way or another.
+    ///    The value obtained transitionValue (initial) can not be negative, the viable values ​​are:
+    ///    
+    ///    + If valueTransition is equal to 0: the active state is maintained.
+    ///    
+    ///    + If valueTransition is greater than 0: the returned value is queried in the transition table
+    ///      and the valueTransition(final) is obtained.
+    ///      
+    ///       - If valueTransition is equal to 0: the active state is maintained.
+    ///       
+    ///       - If valueTransition is less than 0: It is an exit transition and this node is exited 
+    ///         with the value returned with ExitStep that has transitionValue with changed sign as 
+    ///         parameter (returned value will be used by the parent to choose the next state with its
+    ///         transition table). In case the value returned is from the root of the structure this 
+    ///         output value can serve to remember from which state it has left if this is important.
+    ///         
+    ///       - If valueTransition is greater than 0: It is an internal transition so it will change
+    ///         the active node and call OnStart to initialize it before being called in the same 
+    ///         execution cycle.
     /// </summary>
     /// <typeparam name="TData">Data managed by the Machine</typeparam>
     public abstract class Machine<TData> : IStateEvaluator<TData> {
--- State.cs
+++ State.cs
@@ -2,11 +2,14 @@
     /// <summary>
     /// If you have readed <see cref="Machine{TData}"/> this becomes obvious.
     /// 
-    /// Methods:
-    ///  + Next is used to execute the process this state is designed to, the returned value is
-    ///    zero when it don't need to change, positive if it's a value chosen for the same Machine,
-    ///    negative if it's an exception. It should be like this to be decoupled.
+    /// Behavior:
     ///  + OnStart is executed before the first Next is called. It's used to prepare the data.
+    ///  
+    ///  + Next is used to execute the process this state is designed to, the returned value 
+    ///    will be:
+    ///     - Equal to zero: When this state must be maintained as active.
+    ///     - Greater than zero: When this state must exit (different numbers for different 
+    ///       exits that can lead to different state changes at parent node).
     /// </summary>
     /// <typeparam name="TData">Data managed by the State</typeparam>
     public abstract class State<TData> : IStateEvaluator<TData> {

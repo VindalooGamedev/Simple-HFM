f76009153020ec8b6d664f0eb967befd96e91ac6
Domingo Osorio <vindaloo.gamedev@gmail.com>
2019-01-29 18:56:01 +0100

    DecisionTree Implemented
    Waiting to implement the DT guided Behavior Tree

---
+++ HFM-With-DT/DTNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HFM.HFM_With_DT
+{
+    public struct DTNode<TData>
+    {
+        private readonly Func<TData, bool> _condition;
+        private readonly int _condIsTrue, _condIsFalse;
+
+        public DTNode(Func<TData, bool> condition, int condIsTrue, int condIsFalse) {
+            _condition = condition;
+            _condIsTrue = condIsTrue;
+            _condIsFalse = condIsFalse;
+        }
+
+        public int Evaluate(TData data) => (_condition(data)) ? _condIsTrue : _condIsFalse;
+    }
+}
---
+++ HFM-With-DT/DecisionTree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HFM.HFM_With_DT
+{
+    public struct DecisionTree<TData>
+    {
+        private readonly DTNode<TData>[] _nodes;
+        private readonly Action<TData>[] _actions;
+
+        public DecisionTree(DTNode<TData>[] nodes, Action<TData>[] actions) {
+            _nodes = nodes;
+            _actions = actions;
+        }
+
+        public int Evaluate(TData data) {
+            int currNode = 0;
+            do {
+                currNode = _nodes[currNode].Evaluate(data);
+            } while (currNode >= 0);
+            currNode = -currNode;
+            _actions[--currNode]?.Invoke(data);
+            return currNode;
+        }
+    }
+}

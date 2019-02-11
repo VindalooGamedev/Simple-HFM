304a5e331c9b4f0aa97b55055623748303e8f79f
Domingo Osorio <vindaloo.gamedev@gmail.com>
2019-02-11 23:01:23 +0100

    Starting Orthogonality
    Starting to face the implementation of orthogonality on the code I already have, it seems that it will need a lot of work to adapt it.

---
+++ FixedSizeStack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace StateMachinesLab
+{
+    public class FixedSizeStack<T> : IEnumerable<T>, IReadOnlyCollection<T>, ICollection, IEnumerable
+    {
+        private T[] _items;
+        private int _count;
+
+        public FixedSizeStack(int size)
+        {
+            _items = new T[size];
+            _count = 0;
+        }
+
+        public int Count => _count;
+
+        public bool IsSynchronized => false;
+        public object SyncRoot { get; } = new object();
+
+        public T Peek() => _items[_count];
+        public void Push(T item)
+        {
+            _count++;
+            _items[_count] = item;
+        }
+
+        public T Pop()
+        {
+            _count--;
+            return _items[_count +1];
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            if (array == null) throw new ArgumentNullException();
+            if (Count + index >= array.Length - 1) throw new ArgumentException();
+            int elementsToCopy = _items.Length - index;
+            for (int i = 0; i < elementsToCopy; i++)
+                array.SetValue(_items[i], index + i);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < _items.Length; i++)
+                yield return _items[i];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ HSM/Harel/MachineCluster.cs
@@ -0,0 +1,27 @@
+﻿using StateMachinesLab.States;
+
+namespace StateMachinesLab.HSM.Harel
+{
+    public class MachineCluster<TData> : IStateComplete<HarelLogicLayer<TData>>
+    {
+        private readonly Machine<TData>[] _machines;
+
+        public int ExecuteNextStep(HarelLogicLayer<TData> logicLayer)
+        {
+            foreach (var item in _machines)
+                item.ExecuteNextStep(logicLayer);
+            return 0;
+        }
+
+        public void OnStart(HarelLogicLayer<TData> logicLayer)
+        {
+            foreach (var item in _machines)
+                item.OnStart(logicLayer);
+        }
+
+        public void OnExit(HarelLogicLayer<TData> logicLayer) {
+            foreach (var item in _machines)
+                item.OnExit(logicLayer);
+        }
+    }
+}
